93af114d2e9980c3701980c43d5d248847e37595
Your Name <you@example.com>
2024-06-26 22:44:59 -0300

    paginas cadastrar e login atualizados, service (requisições HTTP), models e context

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,27 @@ import NavBar from './components/navBar/NavBar';
 import Footer from './components/footer/Footer';
 import Login from './paginas/login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cadastro from './paginas/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App(){
   
   return (
     <>
-    <BrowserRouter>
-      <NavBar/>
-      <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-          </div>
-      <Footer/>
+    <AuthProvider>
+      <BrowserRouter>
+        <NavBar/>
+        <div className='min-h-[80vh]'>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/home" element={<Home />} />
+              </Routes>
+            </div>
+        <Footer/>
       </BrowserRouter>
+    </AuthProvider>    
     </>
   )
 }
